2d6df649672c939e04fbdb78fc573abe00387aea
RichardHicklin <49760298+RichardHicklin@users.noreply.github.com>
2019-06-12 19:47:25 +0100

    typo......

--- IFAndNewItems-Example2.ps1
+++ IFAndNewItems-Example2.ps1
@@ -1,10 +1,10 @@
 # Its better to test before to save an error!
 # So this example is the one we will use.
 
-# $PathToDirectory = "c:\ps-test\scripts" <- We set this in code. we then test the path to see if the direcotrey is present
+# $PathToDirectory = "c:\ps-test\scripts" <- We set this in code. we then test the path to see if the directory  is present
 # If it is the simple cmd end and just add the txt files.
 # If the path is not present the code will create it using the new-Item
-# The exampke shows a simple If statement
+# The example shows a simple If statement
 
 $PathToDirectory = "c:\ps-test\scripts"
 If(!(test-path $PathToDirectory))
